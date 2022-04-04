c8efa926237c2645d8b5d4389be7a23de55c9966
Pascal Mercat <pascal.mercat@gmail.com>
2022-04-04 17:25:33 +0200

    Netlify fix

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -19,6 +19,7 @@ const MainPage = ({
   const [newCity, setNewCity] = useState();
   const [newComment, setNewComment] = useState();
 
+  const navigate = useNavigate();
   const handleSubmit1 = async (e) => {
     try {
       e.preventDefault();
